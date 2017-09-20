d6e69a07674e1d68bedb7e1b6404fc515b07ae05
Xavier Sellier <xsellier@gmail.com>
2017-09-19 21:10:09 -0400

    Corrige le problème de signal non émit par le node de type Grid

--- Script/data.gd
+++ Script/data.gd
@@ -1,8 +1,8 @@
 extends Node
 
 const MAGASIN = [
-{"nom" : "shop", "price" : 150},
-{"nom" : "ferme", "price" : 400},
-{"nom" : "usine", "price" : 800},
-{"nom" : "cinema", "price" : 1200}
-];
+  {"nom" : "shop", "price" : 150},
+  {"nom" : "ferme", "price" : 400},
+  {"nom" : "usine", "price" : 800},
+  {"nom" : "cinema", "price" : 1200}
+]
